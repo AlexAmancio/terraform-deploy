5e94d0f71e631ac798811249026b9b510fc43c00
Christian Tineo <Christian.tineo.528@gmail.com>
2025-11-20 19:33:17 -0500

    adding cluster

---
+++ modules/01-app-service/aks.tf
@@ -0,0 +1,35 @@
+resource "azurerm_container_registry" "acr" {
+  name                = "${local.naming_convention}-acr"
+  resource_group_name = var.rg_name
+  location            = var.region_zone
+  sku                 = "Standard"
+  admin_enabled       = false
+}
+
+# AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${local.naming_convention}-aks"
+  location            = var.region_zone
+  resource_group_name = var.rg_name
+  dns_prefix          = "${azurerm_kubernetes_cluster.aks.name}-dns"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B4ms"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  # Permite que AKS use el ACR
+  depends_on = [azurerm_container_registry.acr]
+}
+
+
---
+++ modules/01-app-service/roles.tf
@@ -0,0 +1,6 @@
+# Vincular ACR con la identidad del AKS
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
+}
