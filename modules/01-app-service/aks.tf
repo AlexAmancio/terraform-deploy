7e110f2c2e5bb02c65b971e080c943687382ee49
Christian Tineo <Christian.tineo.528@gmail.com>
2025-11-20 19:43:18 -0500

    fixing aks

--- modules/01-app-service/aks.tf
+++ modules/01-app-service/aks.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${local.naming_convention}-aks"
   location            = var.region_zone
   resource_group_name = var.rg_name
-  dns_prefix          = "${azurerm_kubernetes_cluster.aks.name}-dns"
+  dns_prefix          = "${azurerm_kubernetes_cluster.aks.name}-dsdsdns"
 
   identity {
     type = "SystemAssigned"
